fed05bbd269e493c2c8f1ca6db56e174914cc90b
Khanh Chung <kchung1997123@gmail.com>
2020-05-02 22:15:24 -0700

    Implement play function

--- application.py
+++ application.py
@@ -20,4 +20,9 @@ def index():
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
+    session["board"][row][col] = session["turn"]
+    if session["turn"] == "X":
+        session["turn"] = "Y"
+    else:
+        session["turn"] = "X"
     return redirect(url_for("index"))

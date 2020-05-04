b9f53ab4599abd99dc427cc522e50d625028612b
Khanh Chung <kchung1997123@gmail.com>
2020-05-03 21:59:23 -0700

    Implement win detection

--- application.py
+++ application.py
@@ -9,9 +9,22 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+def check_victory(board):
+    print(board)
+    if board[0][0] != None and board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+
+    if board[0][2] != None and board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
+    for i in range(3):
+        if board[i][0] != None and board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] != None and board[0][i] == board[1][i] == board[2][i]:  
+            return board[0][i]
+
 @app.route("/")
 def index():
-
     if "board" not in session:
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
@@ -25,6 +38,11 @@ def play(row, col):
         session["turn"] = "O"
     else:
         session["turn"] = "X"
+
+    winner = check_victory(session["board"])
+    if winner != None:
+        return render_template("game.html", game=session["board"], turn=session["turn"], winner=winner)
+
     return redirect(url_for("index"))
 
 @app.route("/reset")

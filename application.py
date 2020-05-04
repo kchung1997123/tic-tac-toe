65738178a3d090b1b315a27bc8c589793255ae4a
Khanh Chung <kchung1997123@gmail.com>
2020-05-04 13:09:54 -0700

    Implement history tracker

--- application.py
+++ application.py
@@ -9,8 +9,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+history = []
+
 def check_victory(board):
-    print(board)
     if board[0][0] != None and board[0][0] == board[1][1] == board[2][2]:
         return board[0][0]
 
@@ -20,8 +21,17 @@ def check_victory(board):
     for i in range(3):
         if board[i][0] != None and board[i][0] == board[i][1] == board[i][2]:
             return board[i][0]
+
         if board[0][i] != None and board[0][i] == board[1][i] == board[2][i]:  
             return board[0][i]
+
+    return None
+
+def switch_turn():
+    if session["turn"] == "X":
+        session["turn"] = "O"
+    else:
+        session["turn"] = "X"
 
 @app.route("/")
 def index():
@@ -29,24 +39,34 @@ def index():
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
 
-    return render_template("game.html", game=session["board"], turn=session["turn"])
+        if not history:
+            history.append(session["board"])
+    
+    winner = check_victory(session["board"])
+
+    return render_template("game.html", game=session["board"], turn=session["turn"], winner=winner, num_history=len(history))
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
     session["board"][row][col] = session["turn"]
-    if session["turn"] == "X":
-        session["turn"] = "O"
-    else:
-        session["turn"] = "X"
-
-    winner = check_victory(session["board"])
-    if winner != None:
-        return render_template("game.html", game=session["board"], turn=session["turn"], winner=winner)
+    history.append(session["board"]) 
+    switch_turn()
 
     return redirect(url_for("index"))
 
 @app.route("/reset")
 def reset():
-    session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-    session["turn"] = "X"
-    return render_template("game.html", game=session["board"], turn=session["turn"])
+    del session["board"]
+    del session["turn"]
+    history.clear()
+
+    return redirect(url_for("index"))
+
+@app.route("/undo")
+def undo_move():
+    if len(history) > 1:
+        history.pop()
+        session["board"] = history[-1]
+        switch_turn()
+
+    return redirect(url_for("index"))  

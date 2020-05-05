3611d6c64e38d2c7582a4cbf97db60920e4a6507
Khanh Chung <kchung1997123@gmail.com>
2020-05-05 01:44:19 -0700

    Refactor check win implement

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+import math
 
 app = Flask(__name__)
 
@@ -9,23 +10,39 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Keep track history
 history = []
 
-def get_winner(board):
-    if board[0][0] != None and board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
+def get_winner():
+    board = session["board"]
+    winner = None
 
-    if board[0][2] != None and board[0][2] == board[1][1] == board[2][0]:
-        return board[0][2]
+    # Diagonal
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+        winner = board[0][0]
+
+    # Diagonal
+    if board[0][2] == board[1][1] and board[0][2] == board[2][0]:
+        winner = board[0][2]
 
     for i in range(3):
-        if board[i][0] != None and board[i][0] == board[i][1] == board[i][2]:
-            return board[i][0]
+        # Horizontal
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2]:
+            winner = board[i][0]
 
-        if board[0][i] != None and board[0][i] == board[1][i] == board[2][i]:  
-            return board[0][i]
+        # Vertical
+        if board[0][i] == board[1][i] and board[0][i] == board[2][i]:  
+            winner = board[0][i]
 
-    return None
+    # Check open spots
+    open_spots = 0
+    for row in board:
+        open_spots += row.count(None)
+
+    if winner == None and open_spots == 0:
+        return "tie"
+    else:
+        return winner
 
 def switch_turn():
     if session["turn"] == "X":
@@ -42,7 +59,7 @@ def index():
         if not history:
             history.append(session["board"])
     
-    winner = get_winner(session["board"])
+    winner = get_winner()
 
     return render_template("game.html", game=session["board"], turn=session["turn"], winner=winner, num_history=len(history))
 
